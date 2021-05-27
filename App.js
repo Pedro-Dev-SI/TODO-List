b1b0d639bf8d6a5e3c23b5daf11306c886ba398f
Pedro Selvate <pedroselvate72@gmail.com>
2021-05-27 18:42:02 -0300

    commit final

--- App.js
+++ App.js
@@ -1,12 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, KeyboardAvoidingView, Platform, TextInput, TouchableOpacity, Keyboard } from 'react-native';
+import Tasks from './components/Tasks';
+
 
 export default function App() {
+
+  const [task, setTask] = useState();
+
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task]);
+    setTask(null);
+  }
+
+  const completeTask = (index) => {
+    let itemsDeleted = [...taskItems];
+    itemsDeleted.splice(index, 1);
+    setTaskItems(itemsDeleted);
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.sectionTasks}>
+        <Text style={styles.title}>TODO List</Text>
+
+        <View style={styles.items}>
+          {
+            taskItems.map((item, index) => {
+              return(
+                <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                  <Tasks text={item}/>
+                </TouchableOpacity>
+              )
+            })
+          }
+        </View>
+      </View>
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTask}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"Digite sua tarefa..."}
+          value={task}
+          onChangeText={(text) => setTask(text)}
+        />
+
+        <TouchableOpacity onPress={() => handleAddTask(task)}>
+          <View style={styles.addTask}>
+            <Text style={styles.plusSign}>+</Text>
+          </View>
+        </TouchableOpacity>
+
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -14,8 +64,60 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#CCCCCC'
   },
+
+  sectionTasks: {
+    flexDirection: 'column',
+    alignItems: 'center',
+
+  },
+
+  title: {
+    paddingTop: 55,
+    paddingBottom: 40,
+    backgroundColor: '#5A5A5A',
+    width: '100%',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 45,
+    color: '#FFFFFF',
+  },
+
+  items: {
+    marginTop: 40,
+  },
+
+  writeTask: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    width: 258,
+    height: 48,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 50,
+    borderColor: '#5A5A5A',
+    borderWidth: 1,
+  },
+
+  addTask: {
+    width: 35,
+    height: 35,
+    borderRadius: 50,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: "#5a5a5a",
+    borderWidth: 1,
+  }
+
+
 });
---
+++ components/Tasks.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+function Tasks(props) {
+   return (
+      <View style={styles.item}>
+         <View style={styles.itemInside}>
+            <View style={styles.square}/>
+            <Text style={styles.itemText}>{props.text}</Text>
+         </View>
+      </View>
+   )
+}
+
+const styles = StyleSheet.create({
+   item: {
+      backgroundColor: '#5A5A5A',
+      padding: 15,
+      borderRadius: 10,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      marginBottom: 20,
+      minWidth: 330,
+      minHeight: 60,
+   },
+
+   itemInside: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      flexWrap: 'wrap',
+   },
+
+   square: {
+      width: 24,
+      height: 24,
+      backgroundColor: '#FF7777',
+      borderRadius: 5,
+      marginRight: 15,
+   },
+
+   itemText: {
+      fontSize: 18,
+      color: '#FFF',
+   },
+})
+
+export default Tasks;
+
